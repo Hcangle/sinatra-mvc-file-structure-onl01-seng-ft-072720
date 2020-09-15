fda6f2277977f5b3b701e4dcbc017c448a8113d3
Hannah Angle <hannahcangle@gmail.com>
2020-09-15 04:01:28 +0000

    Automatically backed up by Learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,13 +1,17 @@
 class Dog 
   attr_accessor :name, :breed, :age 
   
-  @@all = []
+  DOGS = []
   
   def initialize (name, breed, age)
     @name = name 
     @breed = breed
     @age = age 
-    @@all = all
+    DOGS << self 
+  end 
+  
+  def self.all 
+    DOGS 
   end 
   
 end 
